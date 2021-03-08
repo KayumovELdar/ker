42b5ee9b53235dbebe78d22125951833e1aba81b
KayumovELdar <eldar13121997@mail.ru>
2021-03-08 07:11:00 +0300

    успешный тест ответа new

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,11 @@
 class AnswersController < ApplicationController
+  def new
+    @answer = question.answers.new
+  end
+
+  private
+
+  def question
+    @question||= Question.find(params[:question_id])
+  end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :answer do
+    title { 'MyString_q' }
+    body { 'MyText_q' }
+    question {nil}
   end
 end
