b9d82c726d532a7689482fabd97d609923247bcb
KayumovELdar <eldar13121997@mail.ru>
2021-03-08 05:00:55 +0300

    успешный тест new и show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,16 +2,41 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   describe "GET #index" do
-    it 'populates an array of all questions' do
-      questions = create_list(:question, 3)
+    let(:questions) { create_list(:question, 3) }
 
-      get :index
+    before { get :index}
+
+    it 'populates an array of all questions' do
 
       expect(assigns(:questions)).to match_array(questions)
     end
     it 'renders index view' do
-      get :index
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) {create(:question) }
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+    it 'assigns new question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :new
     end
   end
 end
