402e2d405c69e3141f59693f20ee33c79723cbbe
KayumovELdar <eldar13121997@mail.ru>
2021-03-12 02:37:58 +0300

    пользователь может просматривать список вопросов

--- spec/features/question/index_spec.rb
+++ spec/features/question/index_spec.rb
@@ -5,5 +5,15 @@ feature "просмотр всех вопросов", %q{
   Можно просмотреть весь список вопросов
   } do
     #создание массива вопросов
+    given!(:questions){create_list(:question, 5) }
     #проверка созданных вопросов
+    scenario 'проверка списка вопросов' do
+      #зайти на страницу с списком вопросов
+      visit questions_path
+      save_and_open_page
+
+      questions.count.times do |n|
+        expect(page).to have_content "Заголовок вопроса № #{n+1}."
+      end
+    end
   end
