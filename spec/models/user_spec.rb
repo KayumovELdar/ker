6c16720b916711af73a039500d43f68a16bd9823
KayumovELdar <eldar13121997@mail.ru>
2021-03-13 21:42:31 +0300

    правки

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe User, type: :model do
   let(:author) { create(:user) }
   let(:user) { create(:user) }
   let(:question) { create(:question, user: author) }
-  let(:answer) { create(:answer, question: question, user: author) }
 
   it { should have_many(:answers) }
   it { should have_many(:questions) }
@@ -13,12 +12,9 @@ RSpec.describe User, type: :model do
 
   it 'user is author' do
     expect(author.author?(question)).to be_truthy
-    expect(author.author?(answer)).to be_truthy
   end
 
   it 'user is not author' do
-    question = create(:question)
     expect(user.author?(question)).to be_falsy
-    expect(user.author?(answer)).to be_falsy
   end
 end
