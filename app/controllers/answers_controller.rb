5e9f10c8d9c6fe3ac55cdecf18c6e791cba2e37e
KayumovELdar <eldar13121997@mail.ru>
2021-03-24 04:39:08 +0300

    пользовательможет создать ответ прикрипив файлы

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -26,7 +26,7 @@ class AnswersController < ApplicationController
   private
 
   def find_answer
-    @answer = Answer.find(params[:id])
+    @answer = Answer.with_attached_files.find(params[:id])
   end
 
   def question
@@ -34,6 +34,6 @@ class AnswersController < ApplicationController
   end
 
   def answer_params
-    params.require(:answer).permit(:title, :body)
+    params.require(:answer).permit(:title, :body, files: [])
   end
 end
--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -36,7 +36,7 @@ feature 'The user can create a question', '
       fill_in 'Body', with: 'text text text'
 
       attach_file 'File',["#{Rails.root}/spec/rails_helper.rb","#{Rails.root}/spec/spec_helper.rb"]
-      click_on 'Ask'
+      click_on 'Ask question'
 
       expect(page).to have_link 'rails_helper.rb'
       expect(page).to have_link 'spec_helper.rb'
