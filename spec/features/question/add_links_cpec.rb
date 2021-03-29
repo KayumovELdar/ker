c4ad28dd3fe44be825ba4e62577a457ba4f1e659
KayumovELdar <eldar13121997@mail.ru>
2021-03-29 04:11:33 +0300

    добавление возможности прикреплять несколько ссылок к вопросу и ответу

--- spec/features/question/add_links_cpec.rb
+++ spec/features/question/add_links_cpec.rb
@@ -7,21 +7,25 @@ feature 'User can add links to question', %q{
 } do
 
   given(:user) { create(:user) }
-  given(:gist_url) { 'https://gist.github.com/vkurennov/743f9367caa1039874af5a2244e1b44c' }
+  given(:gist_url1) {'https://gist.github.com/KayumovELdar/4e36eb9f97f59260c0f6d35e69ff6ea2'}
+  given(:gist_url2) {'https://gist.github.com/KayumovELdar/a6c15d72980c611879f8bbb6dee93d09'}
 
   scenario 'User adds link when asks question' do
     sign_in(user)
     visit new_question_path
 
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
+    fill_in 'Link name', with: "My gist1"
+    fill_in 'Url', with: gist_url1
 
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
+    click_on 'add link'
 
+    within all(:css, ".nested-fields")[1] do
+      fill_in 'Link name', with: "My gist2"
+      fill_in 'Url', with: gist_url2
+    end
     click_on 'Ask'
-
-    expect(page).to have_link 'My gist', href: gist_url
+    expect(page).to have_link 'My gist1', href: gist_url1
+    expect(page).to have_link 'My gist2', href: gist_url2
   end
 
 end
