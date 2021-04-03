871cfbf73c1e718d9f9a4e9a957006e33e3b3607
KayumovELdar <eldar13121997@mail.ru>
2021-04-04 01:32:57 +0300

    создание concerns voted

---
+++ app/controllers/concerns/voted.rb
@@ -0,0 +1,27 @@
+module Voted
+  extend ActiveSupport::Concern
+  included do
+    before_action :set_votable, only: %i[vote_up, vote_down, cancel_vote]
+  end
+
+  def vote_up
+    @votable.up(current_user)
+    render json: { id: @votable.id, rating: @votable.rating }
+  end
+
+  def vote_down
+    @votable.down(current_user)
+    render json: { id: @votable.id, rating: @votable.rating }
+  end
+
+  private
+
+  def model_klass
+    controller_name.classify.constantize
+  end
+
+  def set_votable
+    @votable = model_klass.find(params[:id])
+  end
+
+end
